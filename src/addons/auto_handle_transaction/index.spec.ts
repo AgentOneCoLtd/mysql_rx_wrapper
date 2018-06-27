3325a5ed9f8d0cbf19c72df3257f19c16a85f910
Krittanan Pingclasai <kp@kpping.me>
2018-06-27 10:06:29 +0700

    implement autoHandleTransaction

---
+++ src/addons/auto_handle_transaction/index.spec.ts
@@ -0,0 +1,100 @@
+import { of, throwError } from 'rxjs';
+import { queryCommitTransaction } from '../query_commit_transaction';
+import { queryRollbackTransaction } from '../query_rollback_transaction';
+import { queryStartTransaction } from '../query_start_transaction';
+import { autoHandleTransaction } from './index';
+
+jest.mock('../query_commit_transaction');
+jest.mock('../query_rollback_transaction');
+jest.mock('../query_start_transaction');
+
+beforeEach(() => {
+    jest.clearAllMocks();
+});
+
+it('should commit and return result of query', (done) => {
+    const RESULT = 'foobar';
+    const mockConnection = {} as any;
+    const mockQuery = of(RESULT);
+
+    const mockCommit = (queryCommitTransaction as any)
+        .mockReturnValue(of('commit'));
+    const mockRollback = (queryRollbackTransaction as any)
+        .mockReturnValue(of('rollback'));
+    const mockStart = (queryStartTransaction as any)
+        .mockReturnValue(of('start'));
+
+    // HACK: infer mockFunction type
+    if (!(jest.isMockFunction(mockCommit)
+        && jest.isMockFunction(mockRollback)
+        && jest.isMockFunction(mockStart))) {
+
+        throw new Error('MOCK_EXPECTED');
+    }
+
+    autoHandleTransaction(mockConnection, mockQuery).subscribe({
+        next(result) {
+            expect(result)
+                .toBe(RESULT);
+        },
+        error(error) {
+            done.fail(error);
+        },
+        complete() {
+            expect(mockCommit.mock.calls.length)
+                .toBe(1);
+
+            expect(mockRollback.mock.calls.length)
+                .toBe(0);
+
+            expect(mockStart.mock.calls.length)
+                .toBe(1);
+
+            done();
+        },
+    });
+});
+
+it('should rollback and throw error', (done) => {
+    const mockConnection = {} as any;
+    const mockQuery = throwError(new Error('foobar'));
+
+    const mockCommit = (queryCommitTransaction as any)
+        .mockReturnValue(of('commit'));
+    const mockRollback = (queryRollbackTransaction as any)
+        .mockReturnValue(of('rollback'));
+    const mockStart = (queryStartTransaction as any)
+        .mockReturnValue(of('start'));
+
+    // HACK: infer mockFunction type
+    if (!(jest.isMockFunction(mockCommit)
+        && jest.isMockFunction(mockRollback)
+        && jest.isMockFunction(mockStart))) {
+
+        throw new Error('MOCK_EXPECTED');
+    }
+
+    autoHandleTransaction(mockConnection, mockQuery).subscribe({
+        next(_result) {
+            done.fail('should not be called');
+        },
+        error(error) {
+            expect(error)
+                .toBeDefined();
+
+            expect(mockCommit.mock.calls.length)
+                .toBe(0);
+
+            expect(mockRollback.mock.calls.length)
+                .toBe(1);
+
+            expect(mockStart.mock.calls.length)
+                .toBe(1);
+
+            done();
+        },
+        complete() {
+            done.fail('should not be called');
+        },
+    });
+});
---
+++ src/addons/auto_handle_transaction/index.ts
@@ -0,0 +1,26 @@
+import { Connection, PoolConnection } from 'mysql';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, mergeMap } from 'rxjs/operators';
+import { queryCommitTransaction } from '../query_commit_transaction';
+import { queryRollbackTransaction } from '../query_rollback_transaction';
+import { queryStartTransaction } from '../query_start_transaction';
+
+/**
+ * auto handle start, commit, and rollback tracsaction
+ * @param   connection  connection that is used in query
+ * @param   query       query to execute in transaction
+ * @return              result of query
+ */
+export function autoHandleTransaction<T>(connection: Connection | PoolConnection, query: Observable<T>): Observable<T> {
+    return queryStartTransaction({ connection }).pipe(
+        mergeMap(() => query),
+
+        mergeMap((result) =>
+            queryCommitTransaction({ connection }).pipe(
+                () => of(result))),
+
+        catchError((error) =>
+            queryRollbackTransaction({ connection }).pipe(
+                () => throwError(error))),
+    );
+}
