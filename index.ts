1aee5223884bebf56ad3431cf11637ed03a45e14
Krittanan Pingclasai <kp@kpping.me>
2018-06-27 12:11:56 +0700

    export new addons

--- index.ts
+++ index.ts
@@ -3,6 +3,10 @@ export * from './src/cores/connection_end';
 export * from './src/cores/ok_packet';
 export * from './src/cores/pool_get_connection';
 export * from './src/cores/query';
+
+export { autoHandlePoolConnection } from './src/addons/auto_handle_pool_connection';
+export * from './src/addons/auto_handle_transaction';
+export { poolJob } from './src/addons/pool_job';
 export * from './src/addons/query_commit_transaction';
 export * from './src/addons/query_insert';
 export * from './src/addons/query_rollback_transaction';
